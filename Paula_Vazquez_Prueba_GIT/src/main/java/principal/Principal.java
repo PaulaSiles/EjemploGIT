5ac6052e1709310b031d6a8e382ddfffb8e163f6
paume <paume@DESKTOP-I0M4DP8>
2024-01-07 19:04:11 +0100

    Primera semana de trabajo

--- Paula_Vazquez_Prueba_GIT/src/main/java/principal/Principal.java
+++ Paula_Vazquez_Prueba_GIT/src/main/java/principal/Principal.java
@@ -4,7 +4,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Comeinzo de mi programa dificilísimo");
+		
+		System.out.println("Sigo con mi programa dificilísimo");
+		
+		System.out.println("Añado funcionalidades a mi programa dificilísimo");
+		
+		System.out.println("Estoy muy contenta con mi programa dificilísimo");
 	}
 
 }
